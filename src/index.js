c98cbab5145dfdb9c401d083c41eb50012ddc051
Ka3aX16 <77680201+Ka3aX16@users.noreply.github.com>
2021-03-16 03:16:32 +0600

    refactor: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) {
+    return 0
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  array.sort((a, b) => a - b);
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) {
+    return 0
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  array.sort((a, b) => a - b);
+  return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) {
+    return 0
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  let avgNum = 0;
+  for (let i = 0; i < array.length; i++) {
+    avgNum += array[i];
+  }
+  return avgNum / array.length
 }
